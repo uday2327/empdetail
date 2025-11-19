4795e6a3bdb0931b74213080309c167b32fabdd5
Uday <uday23.com.in>
2025-11-19 19:13:33 +0530

    add basic employee details (id and name)

---
+++ emp.py
@@ -0,0 +1,42 @@
+
+
+
+
+employee_id = 101
+employee_name = "John Doe"
+
+print("Employee ID:", employee_id)
+print("Employee Name:", employee_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
