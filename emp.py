f89357e582e664a2390a9dcec9a65d148f199a7f
Uday <uday23.com.in>
2025-11-19 19:20:27 +0530

    Add department and salary details

--- emp.py
+++ emp.py
@@ -1,12 +1,16 @@
 
 
 
-
+emp_department = "IT"
+emp_salary = "5000000"
 employee_id = 101
 employee_name = "John Doe"
 
 print("Employee ID:", employee_id)
 print("Employee Name:", employee_name)
+print("Employee department:", emp_department = "IT"
+)
+print("Employee Salary:",emp_salary)
 
 
 
